10f7784cb3fc26cd1ca4fc5ba198835762b1d532
abhiramtilak <abhiram.veerapanei@ggktech.com>
2019-11-19 17:18:16 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomePageComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
